23e6cffdea3ad12dd1acf43f99ae2d09a6db37d9
Zohar Nyego <zoharngo@gmail.com>
2019-11-30 23:12:32 +0200

    change api server listening port

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://localhost:8888")
+      "process.env.API_URL": JSON.stringify("http://localhost:5000")
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
